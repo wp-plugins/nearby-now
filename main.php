e4458471f03a87835823560cf9599a64bb41c581
azcoov <azcoov@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-01 17:24:15 +0000

    Adding version 1.0 of the Nearby Now plugin
    git-svn-id: https://plugins.svn.wordpress.org/nearby-now/trunk@498684 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ main.php
@@ -0,0 +1,124 @@
+<?php
+	/*
+	Plugin Name: Nearby Now Recent Reviews
+	Plugin URI: http://www.nearbynow.co/wordpress
+	Description: Nearby Now - Recent Reviews Plugin.
+	Version: 0.0.2
+	Author: Nearby Now
+	Author URI: http://www.nearbynow.co
+	*/
+
+	add_action('wp_print_styles', 'add_nearbynow_stylesheet');
+	add_action('wp_enqueue_scripts', 'load_nearbynow_remote_scripts');
+	add_shortcode('recentreviews','get_recent_reviews');
+	add_shortcode('serviceareamap','get_service_area_map');
+
+	function add_nearbynow_stylesheet() {
+	    wp_register_style( 'nearbynow_css', 'https://s3.amazonaws.com/cdn.nearbynow.co/css/nnplugin.css' );
+	    wp_enqueue_style( 'nearbynow_css' );
+	}
+
+	function load_nearbynow_remote_scripts() {
+	    wp_deregister_script('jquery');
+	    wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js');
+	    wp_enqueue_script( 'jquery');
+
+	    wp_register_script( 'nearbynow_map', 'http://maps.google.com/maps/api/js?sensor=false');
+	    wp_enqueue_script( 'nearbynow_map', array('jquery') );
+
+	    wp_register_script( 'nearbynow_heatmap', 'https://s3.amazonaws.com/cdn.nearbynow.co/scripts/heatmap.js');
+	    wp_enqueue_script( 'nearbynow_heatmap', array('nearbynow_map') );
+
+	    wp_register_script( 'nearbynow_heatmap_gmaps', 'https://s3.amazonaws.com/cdn.nearbynow.co/scripts/heatmap-gmaps.js');
+	    wp_enqueue_script( 'nearbynow_heatmap_gmaps', array('nearbynow_heatmap') );
+
+	    wp_register_script( 'nearbynow_map', 'http://maps.google.com/maps/api/js?sensor=false');
+	    wp_enqueue_script( 'nearbynow_map', array('nearbynow_heatmap_gmaps') );
+	}
+
+	function get_recent_reviews($atts) {  
+		$state = urlencode($atts['state']);
+		$city = urlencode($atts['city']);
+		$radius = $atts['radius'];
+		$showMap = $atts['showmap'];
+		$start = $atts['start'];
+		$count = $atts['count'];
+		$zoom = $atts['zoomlevel'];
+		$options = get_option('nearbynow_options');
+		$token = $options['text_string'];
+		$url = "http://api.sidebox.com/plugin/nearbyreviews?storefronttoken=$token&state=$state&city=$city&zoomlevel=$zoom&radius=$radius";
+		$response = wp_remote_get($url);
+		if( is_wp_error( $response ) ) {
+		   echo 'Oops, something went wrong with the Nearby Now plugin';
+		} else {
+		   //$array = json_decode($response['body']);  
+		   print_r( $response['body'] );
+		}
+	}
+
+	function get_service_area_map($atts) {  
+		$state = urlencode($atts['state']);
+		$city = urlencode($atts['city']);
+		$radius = $atts['radius'];
+		$showMap = $atts['showmap'];
+		$start = $atts['start'];
+		$count = $atts['count'];
+		$zoom = $atts['zoomlevel'];
+		$options = get_option('nearbynow_options');
+		$token = $options['text_string'];
+		$url = "http://api.sidebox.com/plugin/nearbyservicearea?storefronttoken=$token&state=$state&city=$city&zoomlevel=$zoom&radius=$radius";
+		$response = wp_remote_get($url);
+		if( is_wp_error( $response ) ) {
+		   echo 'Oops, something went wrong with the Nearby Now plugin';
+		} else {
+		   //$array = json_decode($response['body']);  
+		   print_r( $response['body'] );
+		}
+	}
+
+	function nearbynow_admin() {  
+	    $opt_name = array('api_token' => 'nbn_api_token');
+	    $hidden_field_name = 'nbn_submit_hidden';
+		if(isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
+		    $opt_val = array('api_token' => $_POST[ $opt_name['api_token'] ]);
+		}
+	}
+
+	function nearbynow_admin_actions() {  
+	    add_options_page("NearbyNow", "NearbyNow", 1, "NearbyNow", "nearbynow_options_page");  
+	}
+	  
+	add_action('admin_menu', 'nearbynow_admin_actions');
+
+function nearbynow_options_page() { ?>
+	<div>
+		<form action="options.php" method="post">
+			<?php settings_fields('nearbynow_options'); ?>
+			<?php do_settings_sections('nearbynow'); ?>
+			<input name="Submit" type="submit" value="<?php esc_attr_e('Save Token'); ?>" />
+		</form>
+	</div>
+<?php
+} 
+
+	add_action('admin_init', 'nearbynow_admin_init');
+
+	function nearbynow_admin_init() {
+		register_setting( 'nearbynow_options', 'nearbynow_options', 'nearbynow_options_validate' );
+		add_settings_section('nearbynow_main', 'Nearby Now Settings', 'nearbynow_section_text', 'nearbynow');
+		add_settings_field('nearbynow_text_string', 'API Token', 'nearbynow_setting_string', 'nearbynow', 'nearbynow_main');
+	}
+
+	function nearbynow_section_text() {
+		echo '<p>The API Token is required for the Nearby Now plugin to function. If the token is missing or invalid the plugin will display an emoty string.</p>';
+	}
+
+	function nearbynow_setting_string() {
+		$options = get_option('nearbynow_options');
+		echo "<input id='nearbynow_text_string' name='nearbynow_options[text_string]' size='40' type='text' value='{$options['text_string']}' />";
+	}
+
+	function nearbynow_options_validate($input) {
+		return $input;
+	}
+?>
---
+++ readme.txt
@@ -0,0 +1,75 @@
+=== Plugin Name ===
+Contributors: azcoov
+Tags: location, reviews, checkins, servicepro
+Requires at least: 2.0.2
+Tested up to: 2.1
+Stable tag: 1.0
+
+The Nearby Now Plugin allows you to display your recent checkins and service area heat-map.
+
+== Description ==
+
+Nearby Now for Wordpress is an awesome way to add your live Nearby Now checkins and reviews right to your website. The plugin can be displayed in two ways:
+
+###Recent Reviews
+
+One of the most useful features of Nearby Now is the ability to send your customers a review form before you leave their driveway. Since all reviews are tied to checkins, Nearby Now is able to geocode those views and display them on the Nearby Network map.
+
+The Recent Reviews plugin allows you to display the recent reviews right on your website, including a map, and a listing of the reviews.
+
+Here is an example of what the recent reviews plugin looks like:
+![Nearby Now Recent Reviews Wordpress Plugin](https://s3.amazonaws.com/cdn.nearbynow.co/blog/wordpress_recent_reviews.png)
+
+###Service Area Map &amp; Recent Checkins
+
+If you are using Nearby Now, it means you are building a service area that represents the area you work, and the neighborhoods you service the most. Your service area and heat map are displayed on the Nearby Now network, but we also want you to display the same service area and heat map on your own website or blog.
+
+The Service Area &amp; Recent Checkins plugin was built just for that. A simple way to display your service area, heat map, and recent checkins for your customers to see.
+
+Here is an example of the service area plugin:
+![Nearby Now Service Area Plugin](https://s3.amazonaws.com/cdn.nearbynow.co/blog/wordpress_service_area.png)
+
+###Configuration
+
+Both plugins can be configured to display a certain location (city/state), as well as a map zoom level, a radius around the city, and the number of items you'd like to show. This is very helpful for those businesses that have multiple locations and host a page for each location and would like to display the plugin data in that locations context.
+
+##How do these plugins help you?
+
+The Nearby Now plugins for Wordpress help your SEO in two ways:
+
+- You can create a web page for each city you service with unique reviews and checkins. Google and other search engines prefer that your website has unique content per page. Duplicate and static content is much less valuable, and Nearby Now is a great way to build that unique dynamic content and plug it into your site.
+- The data that is published by the Nearby Now plugin is coded in optimized micro-formats. This helps search engines recognize the value of the reviews and checkins being published.
+
+##How do I get started?
+
+It's really simple. [Download the plugin here](https://s3.amazonaws.com/cdn.nearbynow.co/apps/wordpress/NearbyNow_Plugins.zip), install it like you would any other wordpress plugin, then email us at `wordpress@nearbynow.co` and we will send you back an API token that you'll enter on the Wordpress Settings page.
+
+Once you have a token you can add a plugin to any web page or blog post. For example, if your business operated in Scottsdale, AZ, and you wanted to display your recent reviews for Scottsdale and the greater Phoenix area, you'd add a short-code to your post like this:
+
+`[recentreviews city="Scottsdale" state="AZ" radius="30" count="10" zoomlevel="10"]`
+
+If you have any questions or need any assistance with the plugins, you can email us at `wordpress@nearbynow.co`, or post a message on our [support site](http://help.sidebox.com/anonymous_requests/new) and we'll help you out.
+
+== Installation ==
+
+1. Upload `main.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add your API token in the settings page
+4. Place `[recentreviews city="Scottsdale" state="AZ" radius="30" count="10" zoomlevel="10"]` in your templates
+
+== Frequently Asked Questions ==
+
+= How do I get an API token? =
+
+Email us at wordpress@nearbynow.co and we'll help you out.
+
+== Screenshots ==
+
+1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
+the directory of the stable readme.txt, so in this case, `/tags/4.3/screenshot-1.png` (or jpg, jpeg, gif)
+2. This is the second screen shot
+
+== Changelog ==
+
+= 1.0 =
+* Version 1.0 releaseed!
